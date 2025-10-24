2421e3b3318a7a064742295e30156f73791dc5ed
Enrique Calderon <enriquecalderon@MacBook-Air-33.local>
2025-10-24 07:34:33 -0600

    Initial mesh app

---
+++ atenea/ateneaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ateneaApp.swift
+//  atenea
+//
+//  Created by Enrique Calderon on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ateneaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
